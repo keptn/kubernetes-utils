bbd32f864bacf99dc2a97f86ff98bd6e6c7ebeca
Florian Bacher <florian.bacher@dynatrace.com>
2020-03-30 17:06:55 +0200

    added helm utils

--- go.mod
+++ go.mod
@@ -4,20 +4,34 @@ go 1.13
 
 require (
 	github.com/Azure/go-autorest/autorest v0.10.0
+	github.com/Masterminds/goutils v1.1.0 // indirect
+	github.com/Masterminds/semver v1.5.0 // indirect
+	github.com/Masterminds/sprig v2.22.0+incompatible // indirect
+	github.com/cyphar/filepath-securejoin v0.2.2 // indirect
+	github.com/gbrlsnchs/jwt/v2 v2.0.0 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/gogo/protobuf v1.3.1 // indirect
 	github.com/google/gofuzz v1.1.0 // indirect
 	github.com/googleapis/gnostic v0.4.1 // indirect
 	github.com/gophercloud/gophercloud v0.9.0 // indirect
+	github.com/huandu/xstrings v1.2.0 // indirect
 	github.com/imdario/mergo v0.3.9 // indirect
 	github.com/json-iterator/go v1.1.9 // indirect
+	github.com/keptn/go-utils v0.6.1-0.20200330141040-86c024b3622b
+	github.com/kinbiko/jsonassert v1.0.1 // indirect
+	github.com/magiconair/properties v1.8.1 // indirect
+	github.com/mitchellh/copystructure v1.0.0 // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
 	golang.org/x/time v0.0.0-20191024005414-555d28b269f0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/src-d/go-git.v4 v4.13.1 // indirect
 	k8s.io/api v0.18.0
 	k8s.io/apimachinery v0.18.0
 	k8s.io/client-go v11.0.0+incompatible
+	k8s.io/helm v2.16.5+incompatible
+	k8s.io/klog v1.0.0 // indirect
 	k8s.io/utils v0.0.0-20200327001022-6496210b90e8 // indirect
 	sigs.k8s.io/yaml v1.2.0 // indirect
 )
--- pkg/kubernetes-utils.go
+++ pkg/kubernetes-utils.go
@@ -1,7 +1,10 @@
 package keptnkubeutils
 
 import (
+	"bytes"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -12,15 +15,22 @@ import (
 	"k8s.io/client-go/kubernetes"
 	v1 "k8s.io/client-go/kubernetes/typed/core/v1"
 	"k8s.io/client-go/util/retry"
+	"k8s.io/helm/pkg/chartutil"
+	"k8s.io/helm/pkg/proto/hapi/chart"
+	"k8s.io/helm/pkg/renderutil"
+	"k8s.io/helm/pkg/timeconv"
 
 	appsv1 "k8s.io/api/apps/v1"
 	typesv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	apierr "k8s.io/apimachinery/pkg/api/errors"
+	kyaml "k8s.io/apimachinery/pkg/util/yaml"
 
 	// Initialize all known client auth plugins.
 	_ "github.com/Azure/go-autorest/autorest"
+	"github.com/keptn/go-utils/pkg/api/models"
+	utils "github.com/keptn/go-utils/pkg/api/utils"
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
@@ -319,4 +329,170 @@ func ExecuteCommandInDirectory(command string, args []string, directory string) 
 		return "", fmt.Errorf("Error executing command %s %s: %s", command, strings.Join(args, " "), err.Error())
 	}
 	return string(out), nil
+}
+
+func getHelmChartURI(chartName string) string {
+	return "helm/" + chartName + ".tgz"
+}
+
+// StoreChart stores a chart in the configuration service
+func StoreChart(project string, service string, stage string, chartName string, helmChart []byte, configServiceURL string) error {
+	resourceHandler := utils.NewResourceHandler(configServiceURL)
+
+	uri := getHelmChartURI(chartName)
+	resource := models.Resource{ResourceURI: &uri, ResourceContent: string(helmChart)}
+
+	_, err := resourceHandler.CreateServiceResources(project, stage, service, []*models.Resource{&resource})
+	if err != nil {
+		return fmt.Errorf("Error when storing chart %s of service %s in project %s: %s",
+			chartName, service, project, err.Error())
+	}
+	return nil
+}
+
+// GetChart reads the chart from the configuration service
+func GetChart(project string, service string, stage string, chartName string, configServiceURL string) (*chart.Chart, error) {
+	resourceHandler := utils.NewResourceHandler(configServiceURL)
+
+	resource, err := resourceHandler.GetServiceResource(project, stage, service, getHelmChartURI(chartName))
+	if err != nil {
+		return nil, fmt.Errorf("Error when reading chart %s from project %s: %s",
+			chartName, project, err.Error())
+	}
+
+	ch, err := LoadChart([]byte(resource.ResourceContent))
+	if err != nil {
+		return nil, fmt.Errorf("Error when reading chart %s from project %s: %s",
+			chartName, project, err.Error())
+	}
+	return ch, nil
+}
+
+// LoadChart converts a byte array into a Chart
+func LoadChart(data []byte) (*chart.Chart, error) {
+	return chartutil.LoadArchive(bytes.NewReader(data))
+}
+
+// LoadChartFromPath loads a directory or Helm chart into a Chart
+func LoadChartFromPath(path string) (*chart.Chart, error) {
+	return chartutil.Load(path)
+}
+
+// PackageChart packages the chart and returns it
+func PackageChart(ch *chart.Chart) ([]byte, error) {
+	helmPackage, err := ioutil.TempDir("", "")
+	if err != nil {
+		return nil, fmt.Errorf("Error when packaging chart: %s", err.Error())
+	}
+	defer os.RemoveAll(helmPackage)
+
+	name, err := chartutil.Save(ch, helmPackage)
+	if err != nil {
+		return nil, fmt.Errorf("Error when packaging chart: %s", err.Error())
+	}
+
+	data, err := ioutil.ReadFile(name)
+	if err != nil {
+		return nil, fmt.Errorf("Error when packaging chart: %s", err.Error())
+	}
+	return data, nil
+}
+
+// GetRenderedDeployments returns all deployments contained in the provided chart
+func GetRenderedDeployments(ch *chart.Chart) ([]*appsv1.Deployment, error) {
+
+	renderOpts := renderutil.Options{
+		ReleaseOptions: chartutil.ReleaseOptions{
+			Name:      ch.Metadata.Name,
+			IsInstall: false,
+			IsUpgrade: false,
+			Time:      timeconv.Now(),
+		},
+	}
+	ch.Values.Raw += `
+keptn:
+  project: prj,
+  service: svc,
+  deployment: dpl`
+	renderedTemplates, err := renderutil.Render(ch, ch.Values, renderOpts)
+	if err != nil {
+		return nil, err
+	}
+
+	deployments := make([]*appsv1.Deployment, 0, 0)
+
+	for _, v := range renderedTemplates {
+		dec := kyaml.NewYAMLToJSONDecoder(strings.NewReader(v))
+		for {
+			var dpl appsv1.Deployment
+			err := dec.Decode(&dpl)
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+
+			if IsDeployment(&dpl) {
+				deployments = append(deployments, &dpl)
+			}
+		}
+	}
+
+	return deployments, nil
+}
+
+// GetRenderedServices returns all services contained in the provided chart
+func GetRenderedServices(ch *chart.Chart) ([]*typesv1.Service, error) {
+
+	renderOpts := renderutil.Options{
+		ReleaseOptions: chartutil.ReleaseOptions{
+			Name:      ch.Metadata.Name,
+			IsInstall: false,
+			IsUpgrade: false,
+			Time:      timeconv.Now(),
+		},
+	}
+	ch.Values.Raw += `
+keptn:
+  project: prj,
+  service: svc,
+  deployment: dpl`
+	renderedTemplates, err := renderutil.Render(ch, ch.Values, renderOpts)
+	if err != nil {
+		return nil, err
+	}
+
+	services := make([]*typesv1.Service, 0, 0)
+
+	for _, v := range renderedTemplates {
+		dec := kyaml.NewYAMLToJSONDecoder(strings.NewReader(v))
+		for {
+			var svc typesv1.Service
+			err := dec.Decode(&svc)
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				continue
+			}
+
+			if IsService(&svc) {
+				services = append(services, &svc)
+			}
+		}
+	}
+
+	return services, nil
+}
+
+// IsService tests whether the provided struct is a service
+func IsService(svc *typesv1.Service) bool {
+	return strings.ToLower(svc.Kind) == "service"
+}
+
+// IsDeployment tests whether the provided struct is a deployment
+func IsDeployment(dpl *appsv1.Deployment) bool {
+	return strings.ToLower(dpl.Kind) == "deployment"
 }
