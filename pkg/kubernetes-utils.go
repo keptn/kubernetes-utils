ad3b436aff25288ade5b32ff087a1f8372134020
Florian Bacher <florian.bacher@dynatrace.com>
2020-04-27 10:46:46 +0200

    include output of exec command in error case

--- pkg/kubernetes-utils.go
+++ pkg/kubernetes-utils.go
@@ -303,7 +303,7 @@ func ExecuteCommand(command string, args []string) (string, error) {
 	cmd := exec.Command(command, args...)
 	out, err := cmd.CombinedOutput()
 	if err != nil {
-		return "", fmt.Errorf("Error executing command %s %s: %s\n%s", command, strings.Join(args, " "), err.Error(), string(out))
+		return string(out), fmt.Errorf("Error executing command %s %s: %s\n%s", command, strings.Join(args, " "), err.Error(), string(out))
 	}
 	return string(out), nil
 }
@@ -314,7 +314,7 @@ func ExecuteCommandInDirectory(command string, args []string, directory string) 
 	cmd.Dir = directory
 	out, err := cmd.CombinedOutput()
 	if err != nil {
-		return "", fmt.Errorf("Error executing command %s %s: %s", command, strings.Join(args, " "), err.Error())
+		return string(out), fmt.Errorf("Error executing command %s %s: %s\n%s", command, strings.Join(args, " "), err.Error(), string(out))
 	}
 	return string(out), nil
 }
